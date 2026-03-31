d470414cda63381112548abbca69705131cd6430
starci183 <cuongnvtse160875@gmail.com>
2026-03-31 15:17:09 +0700

    Add useSetCookie hook to UseEffects for cookie management

--- src/hooks/effects/UseEffects.tsx
+++ src/hooks/effects/UseEffects.tsx
@@ -1,9 +1,11 @@
 import React from "react"
 import { useSyncReduxCourseId } from "./useSyncReduxCourseId"
+import { useSetCookie } from "./useSetCookie"
 
 export const UseEffects = () => {
     /** The useEffect to sync the redux course id. */
     useSyncReduxCourseId()
+    useSetCookie()
     return (
         <></>
     )
---
+++ src/hooks/effects/useSetCookie.ts
@@ -0,0 +1,12 @@
+import { useEffect } from "react"
+import { useLocale } from "next-intl"
+
+/**
+ * Set the cookie for the locale.
+ */
+export const useSetCookie = () => {
+    const locale = useLocale()
+    useEffect(() => {
+        document.cookie = `locale=${locale}`
+    }, [locale])
+}
